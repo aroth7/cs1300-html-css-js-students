58a48e1ba100061dcf46ccfbab0c968fd8751acb
Annabel Roth <annabelroth@annabels-mbp.devices.brown.edu>
2022-09-10 17:35:47 -0400

    first web thingy

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -40,14 +40,21 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
 
-  // TODO: Create a new HTML element to display your data 
+  const sugaryFruit = fruitsArray.filter(fruit => {return fruit.nutritions.sugar > 15;})
+  const sugaryFruitNames = sugaryFruit.map(fruit => {return fruit.name;})
+  console.log(sugaryFruit);
+
+  // TODO: Create a new HTML element to display your data
+  const fruitsElement = document.createElement('div');
+  fruitsElement.innerHTML = "The fruits with sugar > 15 are: " + sugaryFruitNames.join(", ")
+
 
   // TODO: Append your new element to the page
+  gallery.append(fruitsElement);
 
 }
 
@@ -55,7 +62,7 @@ const updatePage = async () => {
 const exampleAddElement = () => {
   // Create a new HTML element and set its properties
   const newElement = document.createElement('div');
-  newElement.innerHTML = "this text is inside a div";
+  newElement.innerHTML = "The fruits with sugar > 15 are: " + sugaryFruitNames.join(", ");
 
   // Append the new element to an existing part of the webpage
   const existingElement = document.getElementById('example-id');
